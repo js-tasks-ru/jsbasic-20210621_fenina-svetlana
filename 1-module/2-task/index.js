e4c2e266dffcd3bf860c495fb32aa76c10577255
Svetlana Chenchikova <fenina.svetlana@gmail.com>
2021-06-30 16:54:01 +0300

    “2-task pwd cd '

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,12 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+ function isValid(name) {
+  if (name != '' && name != null && name.includes(' ') === false && name.length >= 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
