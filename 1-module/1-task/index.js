005ad12f4019158000c05fce17f42112dabfa1e4
Svetlana Chenchikova <fenina.svetlana@gmail.com>
2021-07-03 21:31:30 +0300

    1-task factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1; 
+  } else {
+    return n * factorial( n - 1 );
+  }
 }
+    factorial(0); // 1
+    factorial(1); // 1
+    factorial(3); // 6
+    factorial(5); // 120
